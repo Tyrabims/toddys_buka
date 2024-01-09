cf95dd15bd85f3713c596655b09af4901f2c2f3d
Tyrabims <abimbola.anibaba@yahoo.com>
2024-01-09 18:51:17 +0100

    Navbar done

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import { Container, Nav, Navbar } from "react-bootstrap";
+// import Navbar
+//  from "./components/Navbar";
+import Home
+ from "./pages/Home";
+import Menu from "./pages/Menu";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+
+function App() {
+  return (
+    <>
+      <Navbar expand="lg" className="fixed-top bg-body-tertiary shadow">
+        <Container>
+      <Navbar.Brand>
+        <Link path="/" className="navbar-brand text-success fw-semibold"> Toddys Buka
+        </Link>
+        </Navbar.Brand>
+         <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+         <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className="me-auto justify-content-end w-100">
+            <Nav.Link href="/" className="active text-uppercase">
+              Home
+            </Nav.Link>
+            <Nav.Link href="/menu" className="text-uppercase">
+              Menu
+            </Nav.Link>
+            <Nav.Link href="/about" className="text-uppercase">
+              About
+            </Nav.Link>
+            <Nav.Link href="/contact" className="text-uppercase">
+              Contact
+            </Nav.Link>
+          </Nav>
+         </Navbar.Collapse>
+    </Container>
+    </Navbar>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/menu" element={<Menu />} />
+      <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<h1>PAGE NOT FOUND</h1>} />
+    </Routes>
+    </>
+  );
+}
+
+export default App;
